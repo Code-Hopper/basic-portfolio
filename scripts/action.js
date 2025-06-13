e92949c8485084340697aa5a9df20d0bc4e3c5a6
code-hopper <ameykhondekar01@gmail.com>
2025-06-13 20:29:20 +0530

    logic for buttons

--- scripts/action.js
+++ scripts/action.js
@@ -3,3 +3,35 @@ let heroCTABtn = document.querySelector("#heroCTABtn");
 heroCTABtn.addEventListener("click", () => {
     window.scrollBy({ top: window.innerHeight, behavior: "smooth" });
 });
+
+
+// about corousel 
+
+let indexes = [1,2,3,4]
+pointer = 0
+
+let aboutCorouselImage = document.querySelector("#about-corousel")
+
+let prevBtn = document.querySelector("#about-corousel-prev-btn")
+let nextBtn = document.querySelector("#about-corousel-next-btn")
+
+prevBtn.addEventListener("click",prevBtnFuction)
+nextBtn.addEventListener("click",nextBtnFuction)
+
+function prevBtnFuction(){
+    if(pointer != 1){
+        pointer--
+    }
+    changeImageForCorousel(pointer)
+}
+
+function nextBtnFuction(){
+    if(pointer != indexes.length){
+        pointer++
+    }
+    changeImageForCorousel(pointer)
+}
+
+function changeImageForCorousel(index){
+    aboutCorouselImage.setAttribute("src",`./media/about-${index}-img.jpg`)
+}
